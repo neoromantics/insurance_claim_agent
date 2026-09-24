49d1053793c73d178277e1fce3805f29998f64b2
Taiyan Liu <newromantics2001@gmail.com>
2026-09-24 15:04:40 -0400

    style: replace emoji favicon with elegant shield vector logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
         { name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
       ],
       link: [
-        { rel: 'icon', href: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🏥</text></svg>' },
+        { rel: 'icon', href: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path fill="%230A84FF" d="M50 5 L15 20 V50 C15 72 30 88 50 95 C70 88 85 72 85 50 V20 Z"/><path fill="%23FFFFFF" d="M45 65 L30 50 L35 45 L45 55 L65 35 L70 40 Z"/></svg>' },
       ],
     },
   },
